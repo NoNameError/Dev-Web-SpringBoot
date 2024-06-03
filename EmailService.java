95afd3a2a585f3998f77ef02b40bf9b49ef0d65f
paulo19Dev <167635682+paulo19Dev@users.noreply.github.com>
2024-06-03 17:24:41 -0300

    Update EmailService.java

--- EmailService.java
+++ EmailService.java
@@ -3,6 +3,7 @@
 package projeto.java.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,12 +13,20 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void enviarEmail(String destinatario, String assunto, String mensagem){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(destinatario);
-        message.setSubject(assunto);
-        message.setText(mensagem);
-        emailSender.send(message);
+    @Value("${spring.mail.username}")
+    private String remetente;
 
+    public void enviarEmail(String destinatario, String assunto, String mensagem){
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(remetente);
+            message.setTo(destinatario);
+            message.setSubject(assunto);
+            message.setText(mensagem);
+            emailSender.send(message);
+            System.out.println("Email enviado");
+        }catch (Exception e){
+            System.out.println("Erro ao enviar email"+e.getLocalizedMessage());
+        }
     }
 }
