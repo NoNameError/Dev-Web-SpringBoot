4b5b84acccfe9e72b75b17d0247b4528f5c3854e
paulo19Dev <167635682+paulo19Dev@users.noreply.github.com>
2024-06-03 11:02:04 -0300

    Create LoginDTO.java

---
+++ LoginDTO.java
@@ -0,0 +1,37 @@
+package projeto.java.api.dto;
+
+
+import org.springframework.beans.BeanUtils;
+import projeto.java.api.entity.LoginEntity;
+
+public class LoginDTO {
+    private Long id;
+    private String email;
+    private String senha;
+
+    public LoginDTO(LoginEntity usuario) {
+        BeanUtils.copyProperties(usuario, this);
+    }
+    public LoginDTO() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
