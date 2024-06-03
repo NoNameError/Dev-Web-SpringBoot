7a61500be48215ac933b67661d1f5eac009d0871
paulo19Dev <167635682+paulo19Dev@users.noreply.github.com>
2024-06-03 11:08:07 -0300

    Create AuthenticationDTO.java

---
+++ AuthenticationDTO.java
@@ -0,0 +1,22 @@
+package projeto.java.api.dto;
+
+public class AuthenticationDTO {
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
