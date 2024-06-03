7ad17708c4a0c34e9594d75461c29ecfd1247c4e
paulo19Dev <167635682+paulo19Dev@users.noreply.github.com>
2024-06-03 11:02:41 -0300

    Create LoginEntity.java

---
+++ LoginEntity.java
@@ -0,0 +1,86 @@
+package projeto.java.api.entity;
+
+import java.util.Objects;
+
+import org.springframework.beans.BeanUtils;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import projeto.java.api.dto.LoginDTO;
+
+@Entity
+@Table(name="funcionarios")
+public class LoginEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    public LoginEntity(LoginDTO usuario) {
+        BeanUtils.copyProperties(usuario, this);
+    }
+
+    public LoginEntity(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LoginEntity other = (LoginEntity) obj;
+        return Objects.equals(id, other.id);
+    }
+}
