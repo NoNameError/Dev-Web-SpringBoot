50c508bd241536ccfd148ffe6e70d6f957d96a32
paulo19Dev <167635682+paulo19Dev@users.noreply.github.com>
2024-06-03 11:07:31 -0300

    Create AuthController.java

---
+++ AuthController.java
@@ -0,0 +1,28 @@
+// AUTENTICAÇÃO 
+
+package projeto.java.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import projeto.java.api.dto.AuthenticationDTO;
+import projeto.java.api.service.AuthService;
+
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = "/email")
+    public ResponseEntity<?> email( @RequestBody AuthenticationDTO authDto) {
+        return ResponseEntity.ok(authService.login(authDto));
+    }
+}
